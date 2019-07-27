d3cfabf51495943775eec806bf325c0e19a46502
EC2 Default User <ec2-user@ip-172-31-88-188.ec2.internal>
2019-07-27 04:29:01 +0000

    add instance controller

--- ec2-controller.sh
+++ ec2-controller.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo 'usage: ';
-    echo 'bash ec2-controller.sh haibin-instance-name';
-    echo "number of arguments received=$#";
+    echo 'bash ec2-controller.sh instance-name default';
     exit -1;
 fi
 
+PROFILE=$2
 mkdir -p ~/hosts
 
-aws --output text ec2 describe-instances --filters "Name=tag:Name,Values=$1" --query "Reservations[*].Instances[*].[PrivateIpAddress]" > ~/hosts/$1
-
-
+aws --profile $PROFILE --output text ec2 describe-instances --filters "Name=tag:Name,Values=$1" --query "Reservations[*].Instances[*].[PrivateIpAddress]" > ~/hosts/$1
+aws --profile $PROFILE --output text ec2 describe-instances --filters "Name=tag:Name,Values=$1" --query "Reservations[*].Instances[*].[PublicIpAddress]" > ~/hosts/$1.public
+hudl -v -h ~/hosts/$1 -c ~/hosts/$1 -d /home/ubuntu/hosts;
