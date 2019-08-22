c9d7aed591e5240ac30fe9d7970c15f59fae2846
Haibin Lin <linhaibin.eric@gmail.com>
2019-08-22 16:45:54 -0700

    Update byteps.sh

--- byteps.sh
+++ byteps.sh
@@ -1,27 +1,27 @@
 if [ $# -le 3 ]; then
     echo 'usage: ';
-    echo 'bash byteps.sh role      num_gpus server worker ip        port';
-    echo 'bash byteps.sh server    8        1      1      127.0.0.1 1234';
-    echo 'bash byteps.sh scheduler 8        1      1      127.0.0.1 1234';
-    echo 'bash byteps.sh worker    8        1      1      127.0.0.1 1234';
+    echo 'bash byteps.sh num_gpus server worker ip        port role';
+    echo 'bash byteps.sh 8        1      1      127.0.0.1 1234 scheduler';
+    echo 'bash byteps.sh 8        1      1      127.0.0.1 1234 worker';
+    echo 'bash byteps.sh 8        1      1      127.0.0.1 1234 server';
     exit -1;
 fi
 
-export DMLC_ROLE=$1;
-export GPUS=$2;
-export DMLC_NUM_SERVER=$3;
-export DMLC_NUM_WORKER=$4;
-export DMLC_PS_ROOT_URI=$5;
-export DMLC_PS_ROOT_PORT=$6;
+export GPUS=$1;
+export DMLC_NUM_SERVER=$2;
+export DMLC_NUM_WORKER=$3;
+export DMLC_PS_ROOT_URI=$4;
+export DMLC_PS_ROOT_PORT=$5;
+export DMLC_ROLE=$6;
 
 if [ $DMLC_ROLE = 'server' ]; then
   export MXNET_OMP_MAX_THREADS=4
   # 4 threads should be enough for a server
   python /usr/local/byteps/launcher/launch.py
 elif [ $DMLC_ROLE = 'worker' ]; then
-  export NVIDIA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8;
+  export NVIDIA_VISIBLE_DEVICES=0,1; #,2,3,4,5,6,7,8;
   export MXNET_SAFE_ACCUMULATION=1;
-
+  export DMLC_WORKER_ID=0;
   #python /usr/local/byteps/launcher/launch.py \
   #     python run_pretraining_hvd.py --data='~/mxnet-data/bert-pretraining/datasets/*/*/*.train,' \
   #     --data_eval='~/mxnet-data/bert-pretraining/datasets/*/*/*.dev,' --num_steps 1000000        \
