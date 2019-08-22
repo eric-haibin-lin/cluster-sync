613b32c0fe3464c63660f4359d397dea819b038f
Haibin Lin <linhaibin.eric@gmail.com>
2019-08-22 16:18:41 -0700

    Update byteps.sh

--- byteps.sh
+++ byteps.sh
@@ -22,8 +22,16 @@ elif [ $DMLC_ROLE = 'worker' ]; then
   export NVIDIA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8;
   export MXNET_SAFE_ACCUMULATION=1;
 
+  #python /usr/local/byteps/launcher/launch.py \
+  #     python run_pretraining_hvd.py --data='~/mxnet-data/bert-pretraining/datasets/*/*/*.train,' \
+  #     --data_eval='~/mxnet-data/bert-pretraining/datasets/*/*/*.dev,' --num_steps 1000000        \
+  #     --lr 1e-4 --batch_size 4096 --accumulate 1 --raw --short_seq_prob 0 --log_interval 10 \
+  #     --accumulate 1 --model bert_24_1024_16 --batch_size_eval 12
+
+  export EVAL_TYPE=benchmark
   python /usr/local/byteps/launcher/launch.py \
-       python run_pretraining_hvd.py --data='~/mxnet-data/bert-pretraining/datasets/*/*/*.train,' --data_eval='~/mxnet-data/bert-pretraining/datasets/*/*/*.dev,' --num_steps 1000000              --lr 1e-4 --batch_size 4096 --accumulate 1 --raw --short_seq_prob 0 --log_interval 10 --accumulate 1 --model bert_24_1024_16 --batch_size_eval 12
+         /usr/local/byteps/example/mxnet/start_mxnet_byteps.sh \
+         --benchmark 1 --batch-size=32
 
 elif [ $DMLC_ROLE = 'scheduler' ]; then
   python /usr/local/byteps/launcher/launch.py
