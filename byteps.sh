8eebc00c045f98d5a8e16b5a4dc3dcda5c59c973
Haibin Lin <linhaibin.eric@gmail.com>
2019-08-22 18:06:57 -0700

    Update byteps.sh

--- byteps.sh
+++ byteps.sh
@@ -1,8 +1,8 @@
 if [ $# -le 3 ]; then
     echo 'usage: ';
-    echo 'bash byteps.sh num_gpus server worker ip        port role       id';
+    echo 'bash byteps.sh num_gpus server worker ip        port role       id model bs';
     echo 'bash byteps.sh 8        1      1      127.0.0.1 1234 scheduler';
-    echo 'bash byteps.sh 8        1      1      127.0.0.1 1234 worker     0';
+    echo 'bash byteps.sh 8        1      1      127.0.0.1 1234 worker     0  bert_24_1024_16 12';
     echo 'bash byteps.sh 8        1      1      127.0.0.1 1234 server';
     exit -1;
 fi
@@ -14,6 +14,8 @@ export DMLC_PS_ROOT_URI=$4;
 export DMLC_PS_ROOT_PORT=$5;
 export DMLC_ROLE=$6;
 export DMLC_WORKER_ID=$7;
+export MODEL=$8;
+export BS=$9;
 
 if [ $DMLC_ROLE = 'server' ]; then
   export MXNET_OMP_MAX_THREADS=4
@@ -32,7 +34,7 @@ elif [ $DMLC_ROLE = 'worker' ]; then
        python3 run_pretraining_hvd.py --data='~/mxnet-data/bert-pretraining/datasets/*/*/*.train,' \
        --data_eval='~/mxnet-data/bert-pretraining/datasets/*/*/*.dev,' --num_steps 1000000        \
        --lr 1e-4 --batch_size 4096 --accumulate 1 --raw --short_seq_prob 0 --log_interval 10 \
-       --accumulate 1 --model bert_12_768_12 --batch_size_eval 1 --backend byteps
+       --accumulate 1 --model $MODEL --batch_size_eval $BS --backend byteps
 
 elif [ $DMLC_ROLE = 'scheduler' ]; then
   python /usr/local/byteps/launcher/launch.py
