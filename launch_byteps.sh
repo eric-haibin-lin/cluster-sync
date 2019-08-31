131b16943a47d58ebbe0c81e3c0f4e385ff2b2e8
Ubuntu <ubuntu@ip-172-32-37-101.ec2.internal>
2019-08-31 04:27:18 +0000

    update docker

--- launch_byteps.sh
+++ launch_byteps.sh
@@ -1,10 +1,10 @@
-num_worker=31
-num_physical_server=31
-num_server=93
+num_worker=4
+num_physical_server=4
+num_server=12
 root_ip=172.32.37.101
-#server_hosts=server_31
-server_hosts=worker_32
-worker_hosts=worker_32
+server_hosts=server_4
+# server_hosts=worker_4
+worker_hosts=worker_4
 
 # net=bert_24_1024_16
 # batch_size=12
@@ -12,23 +12,23 @@ worker_hosts=worker_32
 byte_part_size=4096000
 byte_num_rings=12
 byte_num_groups=4
-byte_omp=1
 byte_pcie=8
 byte_push=1
+byte_omp=8
 byte_cpu=1
 
 
-server_docker=haibinlin/server:1
-worker_docker=haibinlin/worker:4
+server_docker=haibinlin/server:3
+worker_docker=haibinlin/worker:5
 
 # cleanup
-#hudl -h $server_hosts "docker pull $server_docker"
+# hudl -h $server_hosts "docker pull $server_docker"
 hudl -h $server_hosts 'sudo pkill python'
-#hudl -h $worker_hosts "docker pull $worker_docker"
+# hudl -h $worker_hosts "docker pull $worker_docker"
 hudl -h $worker_hosts 'sudo pkill python3'
 
 # scheduler
-nvidia-docker run -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $server_docker bash -c "cd gluon-nlp/scripts/bert/; bash byteps.sh 8 $num_server $num_worker $root_ip  1234 scheduler 0 bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups"
+nvidia-docker run -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $server_docker bash -c "cd gluon-nlp/scripts/bert/; bash byteps.sh $num_server $num_worker $root_ip  1234 scheduler $byte_part_size $byte_num_rings $byte_num_groups $byte_push $byte_omp $byte_cpu"
 
 num_server_iter=0
 target_server_iter=$num_server/$num_physical_server
@@ -38,8 +38,8 @@ do
   then
     break
   fi
-  hudl -h $server_hosts -t "nvidia-docker run -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $server_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh 8 $num_server $num_worker $root_ip  1234 server 0 bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_omp'"
-  echo "launched $num_physical_server servers"
+  hudl -h $server_hosts -t "nvidia-docker run -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $server_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh $num_server $num_worker $root_ip  1234 server $byte_part_size $byte_num_rings $byte_num_groups $byte_push $byte_omp $byte_cpu'"
+  echo "launched $num_physical_server servers: nvidia-docker run -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $server_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh $num_server $num_worker $root_ip  1234 server $byte_part_size $byte_num_rings $byte_num_groups $byte_push $byte_omp $byte_cpu'"
   let "num_server_iter+=1"
 done;
 
@@ -47,6 +47,7 @@ count=0
 while read -u 10 host;
 do
   host=${host%% slots*}
-  ssh -o "StrictHostKeyChecking no" $host tmux new-session -d "nvidia-docker run --security-opt seccomp=seccomp.json -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $worker_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh 8 $num_server $num_worker $root_ip 1234 worker $count bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_pcie $byte_push $byte_cpu'"
+  echo "nvidia-docker run --security-opt seccomp=seccomp.json -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $worker_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh 8 $num_server $num_worker $root_ip 1234 worker $count bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_pcie' on $host"
+  ssh -o "StrictHostKeyChecking no" $host tmux new-session -d "nvidia-docker run --security-opt seccomp=seccomp.json -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $worker_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh $num_server $num_worker $root_ip 1234 worker $count bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_pcie'"
   let "count+=1"
 done 10<$worker_hosts;
