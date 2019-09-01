c73dae45369fd809a90b95bf99c64a0b21c1ce9a
Ubuntu <ubuntu@ip-172-31-9-2.ec2.internal>
2019-09-01 05:44:30 +0000

    add LB

--- launch_byteps.sh
+++ launch_byteps.sh
@@ -10,16 +10,17 @@ worker_hosts=worker_4
 # batch_size=12
 
 byte_part_size=4096000
-byte_num_rings=12
+byte_num_rings=1
 byte_num_groups=4
 byte_pcie=8
 byte_push=1
-byte_omp=8
+byte_omp=1
 byte_cpu=1
+byte_load_balance=2
 
 
 server_docker=haibinlin/server:3
-worker_docker=haibinlin/worker:5
+worker_docker=haibinlin/worker:7
 
 # cleanup
 # hudl -h $server_hosts "docker pull $server_docker"
@@ -47,7 +48,7 @@ count=0
 while read -u 10 host;
 do
   host=${host%% slots*}
-  echo "nvidia-docker run --security-opt seccomp=seccomp.json -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $worker_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh 8 $num_server $num_worker $root_ip 1234 worker $count bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_pcie' on $host"
-  ssh -o "StrictHostKeyChecking no" $host tmux new-session -d "nvidia-docker run --security-opt seccomp=seccomp.json -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $worker_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh $num_server $num_worker $root_ip 1234 worker $count bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_pcie'"
+  echo "nvidia-docker run --security-opt seccomp=seccomp.json -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $worker_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh $num_server $num_worker $root_ip 1234 worker $count bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_pcie $byte_load_balance' on $host"
+  ssh -o "StrictHostKeyChecking no" $host tmux new-session -d "nvidia-docker run --security-opt seccomp=seccomp.json -d -v ~/.ssh:/root/.ssh --network=host --shm-size=32768m $worker_docker bash -c 'cd gluon-nlp/scripts/bert/; bash byteps.sh $num_server $num_worker $root_ip 1234 worker $count bert_24_1024_16 12 $byte_part_size $byte_num_rings $byte_num_groups $byte_pcie $byte_load_balance'"
   let "count+=1"
 done 10<$worker_hosts;
